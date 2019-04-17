32ef2cd9a9f365c068aabd093f9bfcdac6f0e7e8
burntsugar <rrakky89@gmail.com>
2019-04-18 07:41:50 +1000

    added inheritance drills challenge from Monday

---
+++ term01/week04/challenges/inheritance-drills.rb
@@ -0,0 +1,18 @@
+# The following challenges are to get your practicing your class inheritance definitions. The scenarios are presented for simplicity.
+
+# Implement class definitions and constructors for the following:
+
+# Fruity...
+# 1. Fruit has a sugar level.
+# 2. Apple is a Fruit.
+# 3. Banana is a fruit and has a magnesium level.
+
+# Vehicle things...
+# 1. Vehicle has a make and model.
+# 2. Heavy Vehicle is a Vehical and has a number of wheels.
+# 3. Light Vehicle is a Vehicle and has a passenger capacity.
+# 4. Super Heavy Vehicle is a Heavy Vehicle and has a maximum weight of 100 tonnes.
+
+# Earthy things
+# 1. A Mouse is a Mammal is an Animal is a Earth Dwelling Creature.
+# 2. An Employee is a Human is a Earth Dwelling Creature.
