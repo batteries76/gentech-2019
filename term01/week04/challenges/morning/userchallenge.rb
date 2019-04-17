508c9f1dbccf78c2c3df81f4333bd4819323a84c
burntsugar <rrakky89@gmail.com>
2019-04-18 07:25:04 +1000

    added monday morning challenge

---
+++ term01/week04/challenges/morning/userchallenge.rb
@@ -0,0 +1,17 @@
+# User Challenge!
+
+# A User has a username, preferred display name and a list of interests (array of strings is fine).
+# 1. Define a class called User which contains the attributes given above.
+# 2. Each attribute should be initialised in the constructor.
+# 3. Each attribute should be readable and writable.
+# 4. Instantiate (create) 3 instances of User. Call them user1, user2 and user3.
+# 5. Display the value of each user attribute.
+# 6. Change the preferred display name of user1, and then display the changed value.
+# 7. Define an array called users_array, and then place user1, user2 and user3 into the array.
+# 8. Iterate (loop) through the array and display the value of each user attribute.
+
+# Optional/Beast
+# 1. Create a class called UserInterests which has a list of interests (array of strings is fine).
+# 2. Add methods which allow the list of interests to be read and added to.
+# 3. Modify the User class - incorporate the UserInterests class and remove the existing array.
+
