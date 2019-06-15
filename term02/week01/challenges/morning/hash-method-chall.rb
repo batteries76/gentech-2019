06a0599ae0e88c6fb1e2410b545ad9474265e65a
burntsugar <rrakky89@gmail.com>
2019-06-15 17:44:00 +1000

    added term2 week 1 challenges and Kahoot

---
+++ term02/week01/challenges/morning/hash-method-chall.rb
@@ -0,0 +1,32 @@
+# Hash method challenge
+
+# 1. *Passing a hash to method...*
+#     1. Create a hash containing at least 3 key/value pairs
+#     2. Write a method that takes a hash. The method prints all values in the hash.
+#     3. Call the method and pass the hash.
+
+
+# 2. *Initialise an object using a hash...*
+
+#     1. Create a class called Foo which has the following instance variables:
+#         * x
+#         * y
+#         * z
+#     2. The constructor takes a hash argument which is used to initialise each instance variable.
+#     3. Create 3 instances of Foo - each with unique attribute values.
+
+
+# 3. *Select hash values conditionally...*
+
+#     1. Create a hash which contains player names and their scores...
+#         * straven - 100
+#         * kidd_thunda - 99
+#         * rakky - 98
+#         * killer_emmy - 97 
+#         * space_toy 50
+
+#     2. Write a method that takes the player score hash. 
+#     3. Use .select to return all players with a score greater than or equal to 99. https://ruby-doc.org/core-2.5.1/Hash.html#method-i-select
+#     4. Print the result.
+    
+
